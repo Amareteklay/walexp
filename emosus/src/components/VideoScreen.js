f7be59f88f540f313e544af5eda5c9f74aaf9861
Amare <amareteklay@gmail.com>
2024-07-15 13:53:52 +0200

    Deploy React app with videos and emojis

--- emosus/src/components/VideoScreen.js
+++ emosus/src/components/VideoScreen.js
@@ -1,41 +1,53 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react"
 
 function VideoScreen({ onReaction, onNext }) {
-  const videoRef = useRef(null);
+  const videoRef = useRef(null)
 
   useEffect(() => {
-    const video = videoRef.current;
-    video.play();
-    document.documentElement.requestFullscreen();
+    const video = videoRef.current
+    video.play()
+    document.documentElement.requestFullscreen()
     return () => {
-      video.pause();
-      video.currentTime = 0;
-      document.exitFullscreen();
-    };
-  }, []);
+      video.pause()
+      video.currentTime = 0
+      document.exitFullscreen()
+    }
+  }, [])
 
   const handleReaction = (reaction) => {
-    const timestamp = videoRef.current.currentTime;
-    onReaction({ reaction, timestamp });
-  };
+    const timestamp = videoRef.current.currentTime
+    onReaction({ reaction, timestamp })
+  }
 
   return (
     <div className="screen">
       <div className="video-container">
         <video ref={videoRef} loop>
-          <source src="assets/Floods1.mp4" type="video/mp4" />
+          <source src="videos/Floods1.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <div className="overlay-text">Video playing now</div>
       </div>
       <div className="emoji-reactions">
-        <img src="assets/emoji-icons/happy.png" alt="Happy" onClick={() => handleReaction('happy')} />
-        <img src="assets/emoji-icons/sad.png" alt="Sad" onClick={() => handleReaction('sad')} />
-        <img src="assets/emoji-icons/neutral.png" alt="Neutral" onClick={() => handleReaction('neutral')} />
+        <img
+          src="emojis/happy.png"
+          alt="Happy"
+          onClick={() => handleReaction("happy")}
+        />
+        <img
+          src="emojis/sad.png"
+          alt="Sad"
+          onClick={() => handleReaction("sad")}
+        />
+        <img
+          src="emojis/neutral.png"
+          alt="Neutral"
+          onClick={() => handleReaction("neutral")}
+        />
       </div>
       <button onClick={onNext}>Next</button>
     </div>
-  );
+  )
 }
 
-export default VideoScreen;
+export default VideoScreen
