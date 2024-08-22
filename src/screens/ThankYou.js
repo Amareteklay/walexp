84f92f01192939e80ce465845397988a4cba0d04
Amare <amareteklay@gmail.com>
2024-08-22 16:23:11 +0200

    experiment complete msg

--- src/screens/ThankYou.js
+++ src/screens/ThankYou.js
@@ -4,14 +4,18 @@ import { styled } from "@mui/system"
 
 const ThankYouContainer = styled(Container)({
   textAlign: "center",
-  minWidth: '60%',
-  minHeight: '80vh',
+  minWidth: "60%",
+  minHeight: "80vh",
   marginTop: "30px",
 })
 
 function ThankYou() {
   useEffect(() => {
     const timer = setTimeout(() => {
+      // Send a message to the parent window (PsychoJS) indicating the experiment is complete
+      window.parent.postMessage({ type: "experiment_complete" }, "*")
+
+      // Optional: Display an alert or any other final message
       alert("Experiment complete. Thank you!")
     }, 5000)
     return () => clearTimeout(timer)
